f6688ab8505646ba31d7ae0b8d9e6bcf19e90f47
Gyuhyeon <weanother@gmail.com>
2021-04-21 16:02:32 +0900

    Basic input event handling to close the app using the escape key. Keycodes used by inside the MACOS is in macos_keycode.h.

--- platform_macos/macos_main.mm
+++ platform_macos/macos_main.mm
@@ -1,53 +1,87 @@
+/*
+    OKAY, here are the most basic things to do inside the MACOS platform layer.
+
+    1. Gamepad input
+    2. Sound
+    3. more robust input handling. keyboard, mouse..
+    4. unified game memory
+    5. DEBUG file I/O
+    6. Framerate control
+    7. live code editing
+    8. input recording
+
+*/
+
 #include <Cocoa/Cocoa.h> 
 #include <OpenGL/gl.h>
+#include <stdint.h>
 
+#define i8 int8_t 
+#define i16 int16_t
+#define i32 int32_t 
+#define i64 int64_t 
+
+#define b32 int32_t
+
+#define u8 uint8_t 
+#define u16 uint16_t 
+#define u32 uint32_t 
+#define u64 uint64_t 
+
+#define r32 float
+#define r64 double
+
+#define global_variable static
+#define internal static
+#define local_function static
+
+global_variable b32 IsGameRunning;
+
+#include "macos_keycode.h"
 #include "macos_soma.cpp"
 
-@interface fox_app_delegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
-@end
-
-// NOTE : Whenever the application steps into [NSApp Run] method, 
-// we are unable to control the application ourselves. Therfore,
-// we need our version of app delegate so that the OS can call this function
-// to 'stop' the application from running.
-@implementation fox_app_delegate : NSObject
-// -(void)applicationWillFinishLaunching: (NSNotification *)notification
-- (void)applicationDidFinishLaunching: (NSNotification *)notification 
+void
+MacOSHandleEvents(NSApplication *App)
 {
-    // This 'stop' function should only be called here - because the application
-    // cannot be manipulated by ourseleves when the [NSApp Run] function is called.
-    [NSApp stop: nil];
-    // Post empty event: without it we can't put application to front
-    // for some reason (I get this technique from GLFW source).
-    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-    NSEvent* event =
-        [NSEvent otherEventWithType: NSApplicationDefined
-                 location: NSMakePoint(0, 0)
-                 modifierFlags: 0
-                 timestamp: 0
-                 windowNumber: 0
-                 context: nil
-                 subtype: 0
-                 data1: 0
-                 data2: 0];
-    [NSApp postEvent: event atStart: YES];
-    [pool drain];
-}
+    // TODO : Check if this loop has memory leak.
+    while(1)
+    {
+        NSEvent *Event = [App nextEventMatchingMask:NSAnyEventMask
+                         untilDate:nil
+                            inMode:NSDefaultRunLoopMode
+                           dequeue:YES];
+        if(Event)
+        {
+            switch([Event type])
+            {
+                case NSEventTypeKeyUp :
+                case NSEventTypeKeyDown :
+                {
+                    u16 KeyCode = [Event keyCode];
+                    if(KeyCode == MACOS_Escape)
+                    {
+                        IsGameRunning = false;
+                    }
+                }break;
 
-- (void)dealloc 
-{
-    [super dealloc];
+                default :
+                {
+                    [App sendEvent : Event];
+                }
+            }
+        }
+        else
+        {
+            // TODO : Make sure this does not 'block' the code.
+            break;
+        }
+    }
 }
-@end
 
 int main(int argc, char **argv)
 {
     NSApplication *App = [NSApplication sharedApplication]; // Your program should invoke this method as one of the first statements in main();
     [App setActivationPolicy :NSApplicationActivationPolicyRegular]; // The application is an ordinary app that appears in the Dock and may have a user interface.
-
-    NSFileManager *FileManager = [NSFileManager defaultManager];
-    NSString *BundlePath = [[NSBundle mainBundle] bundlePath];
-	NSString *ResourcePath = [NSString stringWithFormat:@"%@/Contents/Resources", BundlePath];
 
     NSMenu *AppMainMenu = [NSMenu alloc];
     NSMenuItem *MenuItemWithAppName = [NSMenuItem new];
@@ -62,12 +96,8 @@ int main(int argc, char **argv)
 	[SubMenuOfMenuItemWithAppName addItem:quitMenuItem];
 	[MenuItemWithAppName setSubmenu:SubMenuOfMenuItemWithAppName];
 
-
-    //- (instancetype)initWithTitle:(NSString *)title;
-    //- (void)addItem:(NSMenuItem *)newItem;
-
-	int BufferWidth = 960;
-	int BufferHeight = 540;
+	int BufferWidth = 960*2;
+	int BufferHeight = 540*2;
     int BytesPerPixel = 4;
     int BufferPitch = BufferWidth * BytesPerPixel;
     void *GameBufferMemory = malloc(BufferPitch * BufferHeight);
@@ -82,53 +112,77 @@ int main(int argc, char **argv)
             Column < BufferWidth;
             ++Column)
         {
-            *Pixel++ = (0xff00ffff);
+            if(Column == BufferWidth/2 ||
+            Row == BufferHeight/2)
+            {
+                *Pixel++ = (0xffff00ff);
+            }
+            else
+            {
+                *Pixel++ = (0xff00ffff);
+            }
         }
 
         FirstPixelOfRow += BufferPitch;
     }
 
-	NSRect WindowFrameRect = NSMakeRect(100.0f, 100.0f, (float)BufferWidth, (float)BufferHeight);
+    // TODO : Don't know why, but MacOS creates a window with doubled size, so request the window to
+    // be half the size at the first place. Maybe there's a proper way to fix this?
+	NSRect WindowFrameRect = NSMakeRect(100.0f, 100.0f, (float)BufferWidth/2, (float)BufferHeight/2);
 
-	NSWindow *window = [[NSWindow alloc] initWithContentRect : WindowFrameRect
+	NSWindow *Window = [[NSWindow alloc] initWithContentRect : WindowFrameRect
 											// Apple window styles : https://developer.apple.com/documentation/appkit/nswindow/stylemask
 											styleMask : NSTitledWindowMask|NSWindowStyleMaskClosable|NSWindowStyleMaskResizable
 											backing : NSBackingStoreBuffered 
 											defer : NO];
 
 	NSString *AppName = [[NSProcessInfo processInfo]processName];
-	[window setTitle:AppName];
-	[window makeKeyAndOrderFront:0];
+	[Window setTitle:AppName];
+	[Window makeKeyAndOrderFront:0];
+    // [Window toggleFullScreen:0]; // TODO : Fullscreen support?
+
 	fox_app_delegate *appDelegate = [[fox_app_delegate alloc] init];
 	[App setDelegate:appDelegate];
-	[window setDelegate:appDelegate];
+	//[Window setDelegate:appDelegate]; // TODO : Do we really need this?
 
-    GLuint TextureID = PrepareDisplayingWithOpenGL(Window, &WindowFrameRect, BufferWidth, BufferHeight, GameBufferMemory); 
+    NSWindow *MainWindow = [App mainWindow];
+
+    macos_opengl_info MacOSOpenGLInfo = {};
+    MacOSOpenGLInfo = PrepareDisplayingWithOpenGL(Window, &WindowFrameRect, BufferWidth, BufferHeight, GameBufferMemory); 
+
+    IsGameRunning = true; // TODO : Put this inside the event handling loop
 
 	[App activateIgnoringOtherApps:YES];
 	[App run];
 
-    while(1)
+    while(IsGameRunning)
     {
-			// CGLLockContext(macosOpenGLInfo.cglContext);
-            {
-		        glClear(GL_COLOR_BUFFER_BIT);
-	            glViewport(0, 0, BufferWidth, BufferHeight);
-		        glBindTexture(GL_TEXTURE_2D, TextureID);
-			    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, BufferWidth, BufferHeight,
-						0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, GameBufferMemory);
-		        glBegin(GL_QUADS); 
-		        {
-		            glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0f, 1.0f);
-		            glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.0f, 1.0f);
-		            glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.0f,  -1.0f);
-		            glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0f,  -1.0f);
-		        } glEnd();
-		        glBindTexture(GL_TEXTURE_2D, 0);
+        MacOSHandleEvents(App);
 
-			    [OpenGLContext flushBuffer];
-            }
-		    // CGLUnlockContext(macosOpenGLInfo.cglContext);
+        // Rendering loop with OpenGL.
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glBindTexture(GL_TEXTURE_2D, MacOSOpenGLInfo.TextureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, 
+                        GL_RGBA8, 
+                        BufferWidth, BufferHeight,
+                        0, 
+                        GL_BGRA, 
+                        GL_UNSIGNED_INT_8_8_8_8_REV, 
+                        GameBufferMemory);
+            glViewport(0, 0, BufferWidth, BufferHeight);
+
+            glBegin(GL_QUADS); 
+            {
+                glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0f, 1.0f);
+                glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.0f, 1.0f);
+                glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.0f,  -1.0f);
+                glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0f,  -1.0f);
+            } glEnd();
+            glBindTexture(GL_TEXTURE_2D, 0);
+
+            [MacOSOpenGLInfo.OpenGLContext flushBuffer]; // This will call glFlush() internally
+        }
     }
 
     return 0;
--- platform_macos/macos_soma.cpp
+++ platform_macos/macos_soma.cpp
@@ -1,16 +1,61 @@
+struct macos_opengl_info
+{
+    GLuint TextureID;
+    NSOpenGLContext *OpenGLContext;
+};
+
+@interface fox_app_delegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+@end
+
+// NOTE : Whenever the application steps into [NSApp Run] method, 
+// we are unable to control the application ourselves. Therfore,
+// we need our version of app delegate so that the OS can call this function
+// to 'stop' the application from running.
+@implementation fox_app_delegate : NSObject
+// -(void)applicationWillFinishLaunching: (NSNotification *)notification
+- (void)applicationDidFinishLaunching: (NSNotification *)notification 
+{
+    // This 'stop' function should only be called here - because the application
+    // cannot be manipulated by ourseleves when the [NSApp Run] function is called.
+    [NSApp stop: nil];
+
+    // Post empty event: without it we can't put application to front
+    // for some reason (I get this technique from GLFW source).
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    NSEvent* event =
+        [NSEvent otherEventWithType: NSApplicationDefined
+                 location: NSMakePoint(0, 0)
+                 modifierFlags: 0
+                 timestamp: 0
+                 windowNumber: 0
+                 context: nil
+                 subtype: 0
+                 data1: 0
+                 data2: 0];
+    [NSApp postEvent: event atStart: YES];
+    [pool drain];
+}
+
+- (void)dealloc 
+{
+    [super dealloc];
+}
+@end
+
 static CVReturn 
 DisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime,
                 CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
 {
-    // CVReturn result = [(__bridge fox_openGL_view*)displayLinkContext getFrameForTime:outputTime];
-    // return result;
+    // CVReturn Result = [(__bridge fox_openGL_view*)displayLinkContext getFrameForTime:outputTime];
+    // return Result;
     return kCVReturnSuccess;
 }
 
-static void
-SetDisplayWithOpenGL(NSWindow *Window, NSRect *WindowFrameRect,
-                    int BufferWidth, int BufferHeight, int GameBufferMemory)
+static macos_opengl_info
+PrepareDisplayingWithOpenGL(NSWindow *Window, NSRect *WindowFrameRect,
+                    int BufferWidth, int BufferHeight, void *GameBufferMemory)
 {
+    macos_opengl_info Result = {};
     NSOpenGLPixelFormatAttribute OpenGLPixelFormatAttributes[] = 
         {NSOpenGLPFADoubleBuffer, 
         NSOpenGLPFAAccelerated,
@@ -19,7 +64,7 @@ SetDisplayWithOpenGL(NSWindow *Window, NSRect *WindowFrameRect,
         0};
     
     NSOpenGLPixelFormat *OpenGLPixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes : OpenGLPixelFormatAttributes];
-    NSOpenGLContext *OpenGLContext = [[NSOpenGLContext alloc] initWithFormat : OpenGLPixelFormat shareContext : 0];
+    Result.OpenGLContext = [[NSOpenGLContext alloc] initWithFormat : OpenGLPixelFormat shareContext : 0];
     NSOpenGLView *OpenGLView = [[NSOpenGLView alloc] initWithFrame : *WindowFrameRect 
                                                     pixelFormat : OpenGLPixelFormat];
 
@@ -29,24 +74,26 @@ SetDisplayWithOpenGL(NSWindow *Window, NSRect *WindowFrameRect,
     {
         // TODO : Double-check whether setting a output callback function even matters.
         CVDisplayLinkSetOutputCallback(DisplayLink, DisplayLinkCallback, 0); 
-        CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(DisplayLink, [OpenGLContext CGLContextObj], [OpenGLPixelFormat CGLPixelFormatObj]);
+        CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(DisplayLink, [Result.OpenGLContext CGLContextObj], [OpenGLPixelFormat CGLPixelFormatObj]);
         CVDisplayLinkStart(DisplayLink);
     }
-    [OpenGLContext makeCurrentContext];
+    [Result.OpenGLContext makeCurrentContext];
 	[Window setContentView:OpenGLView];
-	[OpenGLView setOpenGLContext:OpenGLContext];
-	[OpenGLContext setView:OpenGLView];
+	[OpenGLView setOpenGLContext : Result.OpenGLContext];
+	[Result.OpenGLContext setView:OpenGLView];
     
-	GLuint TextureID;
+    /*
+        Set texture info
+    */
     glEnable(GL_TEXTURE_2D);
     glDisable(GL_DEPTH_TEST);
     // GL_UNPACK_ALIGNMENT affects how to unpack the given texture(in out case, our output buffer)
     // TODO : why 1 byte alignment?
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &TextureID);
-    glBindTexture(GL_TEXTURE_2D, TextureID);
+    glGenTextures(1, &Result.TextureID);
 
+    glBindTexture(GL_TEXTURE_2D, Result.TextureID);
     glTexImage2D(GL_TEXTURE_2D, 0, 
                 GL_RGBA8, 
                 BufferWidth, BufferHeight,
@@ -59,16 +106,7 @@ SetDisplayWithOpenGL(NSWindow *Window, NSRect *WindowFrameRect,
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-    GLenum error = glGetError();
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-
-
+    return Result;
 }
-
-
